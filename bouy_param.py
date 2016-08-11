b8e36059d82c0c1ca3995b464f3252845a800a33
Jonathan Robey <class4kayaker@gmail.com>
2016-08-10 18:55:12 -0700

    Rework structure

--- bouy_param.py
+++ bouy_param.py
@@ -1,80 +1,171 @@
 import sys
-import re
 import sympy
 
 
-class BouyParam:
+class NonDim:
     def __init__(self):
         self.constants = {}
-        self.constants[sympy.symbols('mu')] = "Viscosity"
-        self.constants[sympy.symbols('rho_0')] = 'Reference density'
-        self.constants[sympy.symbols('k')] = "Thermal conductivity"
-        self.constants[sympy.symbols('alpha_v')] = (
-            'Thermal expansion coefficient'
-        )
-        self.constants[sympy.symbols('c_p')] = 'Reference specific heat'
-        self.constants[sympy.symbols('Delta_rho')] = (
-            'Density differential for compositional field 1'
-        )
-        self.constants[sympy.symbols('b')] = 'Y extent'
-        self.constants[sympy.symbols('T_h')] = 'Bottom temperature'
-        self.constants[sympy.symbols('T_l')] = 'Top temperature'
-        self.constants[sympy.symbols('g')] = 'Magnitude'
-
-        self.values = {}
-        for k in self.constants:
-            self.values[k] = float('nan')
-
         self.nondim = {}
-        self.nondim['Ra'] = sympy.S(
-            'g*rho_0^2*alpha_v*(T_h-T_l)*b^3*c_p/(mu*k)'
-        )
-        self.nondim['B'] = sympy.S('Delta_rho/(rho_0*alpha_v*(T_h-T_l))')
-        self.nondim['Pr'] = sympy.S('c_p*mu/rho_0')
+        pass
 
-    def print_values(self):
-        for k in self.values:
-            print "{:15}={:13g}".format(sympy.latex(k), self.values[k])
+    def add_constant(self, var, descrip):
+        if not isinstance(var, sympy.Symbol):
+            raise ValueError("Argument var must be a sympy Symbol")
+        if var not in self.constants:
+            self.constants[var] = descrip
+        else:
+            raise ValueError(sympy.string(var)+" already exists")
 
-    def print_nondim(self):
+    def add_nondim_param(self, label, expr):
+        self.nondim[label] = expr
+
+    def print_constants(self, subs_list, verb=False):
+        for v, n in subs_list:
+            if v in self.constants:
+                tex_var = sympy.latex(v)
+                if verb:
+                    defn = self.constants[v]
+                    print("{:15}= {:13g}: {}".format(tex_var, n, defn))
+                else:
+                    print("{:15}= {:13}".format(tex_var, n))
+
+    def print_nondim(self, subs_list, verb=False):
         for c in self.nondim:
             expr = self.nondim[c]
-            val = expr.subs(self.values.items())
+            val = expr.subs(subs_list)
             if isinstance(val, sympy.Float):
-                print "{:5}={:13g}={}".format(c, float(val), sympy.latex(expr))
+                if(verb):
+                    print("{:5}={:13e}={}".format(c,
+                                                  float(val),
+                                                  sympy.latex(expr)))
+                else:
+                    print("{:5}={:13e}".format(c,
+                                               float(val)))
             else:
-                print "{:5}={:13}={}".format(c, float(val), sympy.latex(expr))
-
-    def set_value(self, var, value):
-        if var not in self.values:
-            raise ValueError("{} not allowed".format(var))
-
-        self.values[var] = value
-
-    def read_params(self, fstr):
-        for line in fstr:
-            for k in self.constants:
-                reexpr = '\\s+set\\s+{}\\s+=\\s+([\d.Ee-]+)'.format(
-                    self.constants[k]
-                )
-                regex = re.compile(reexpr)
-                match = regex.match(line)
-                if match is not None:
-                    try:
-                        self.values[k] = float(match.groups()[0])
-                    except:
-                        pass
+                if(verb):
+                    print("{:5}={}={}".format(c, val, sympy.latex(expr)))
+                else:
+                    print("{:5}={}".format(c, val))
 
 
-class ParsePRM:
+class SearchPRM:
     def __init__(self):
-        self.contents = {}
+        self.constants = {}
+
+    def add_constant_location(self, var, search):
+        if not isinstance(var, sympy.Symbol):
+            raise TypeError("Argument var must be a sympy Symbol")
+        if not isinstance(search, str):
+            raise TypeError("Argument search must be a string")
+        if search in self.constants:
+            self.constants[search].append(var)
+        else:
+            self.constants[search] = [var]
+
+    def get_prm_vals(self, prmfile):
+        values = {}
+        position = []
+        parse_line = ""
+        for line in prmfile:
+            # Strip comments
+            c_idx = line.find('#')
+            if(c_idx >= 0):
+                line = line[:c_idx]
+
+            parse_line += line.strip()
+
+            if(parse_line[-1:] == '\\'):
+                continue
+
+            if not parse_line:
+                continue
+
+            if(parse_line == 'end'):
+                if(len(position) > 0):
+                    position.pop()
+                else:
+                    raise ValueError("Invalid prm file")
+                parse_line = ""
+                continue
+
+            if ' ' in parse_line:
+                command, remainder = parse_line.split(' ', 1)
+            else:
+                raise ValueError("Bad line: "+parse_line)
+
+            parse_line = ""
+
+            if(command == 'subsection'):
+                position.append(remainder.strip())
+            elif(command == 'set'):
+                key, value = remainder.split('=', 1)
+                position.append(key.strip())
+                search = ':'.join(position)
+                position.pop()
+                if search in self.constants:
+                    for k in self.constants[search]:
+                        sval = value.strip()
+                        try:
+                            values[k] = float(sval)
+                        except:
+                            values[k] = sval
+            else:
+                errmsg = "Bad command {} with arg {}".format(command,
+                                                             remainder)
+                raise ValueError(errmsg)
+
+        return(values)
+
+BouyParam = NonDim()
+BouySearch = SearchPRM()
+prm_const_dict = {
+    sympy.symbols('mu'):
+    ('Viscosity', "Material model:Simple model:Viscosity"),
+    sympy.symbols('rho_0'):
+    ('Reference density', 'Material model:Simple model:Reference density'),
+    sympy.symbols('k'):
+    ("Thermal conductivity",
+     "Material model:Simple model:Thermal conductivity"),
+    sympy.symbols('alpha_v'):
+    ('Thermal expansion coefficient',
+     "Material model:Simple model:Thermal expansion coefficient"),
+    sympy.symbols('c_p'):
+    ('Reference specific heat',
+     "Material model:Simple model:Reference specific heat"),
+    sympy.symbols('Delta_rho'):
+    ('Density variation',
+     'Material model:Simple model:Density '
+     'differential for compositional field 1'),
+    sympy.symbols('b'):
+    ('Depth', 'Geometry model:Box:Y extent'),
+    sympy.symbols('T_h'):
+    ('Core temp', 'Boundary temperature model:Box:Bottom temperature'),
+    sympy.symbols('T_l'):
+    ('Surface temp', 'Boundary temperature model:Box:Top temperature'),
+    sympy.symbols('g'):
+    ('Gravity', 'Gravity model:Vertical:Magnitude')
+}
+nondim_dict = {
+    'Ra':
+    sympy.S('g*rho_0^2*alpha_v*(T_h-T_l)*b^3*c_p/(mu*k)'),
+    'B':
+    sympy.S('Delta_rho/(rho_0*alpha_v*(T_h-T_l))'),
+    'Pr':
+    sympy.S('c_p*mu/rho_0')
+}
+for k in prm_const_dict:
+    (defn, loc) = prm_const_dict[k]
+    BouyParam.add_constant(k, defn)
+    BouySearch.add_constant_location(k, loc)
+for k in nondim_dict:
+    expr = nondim_dict[k]
+    BouyParam.add_nondim_param(k, expr)
 
 
 for f in sys.argv[1:]:
-    prms = BouyParam()
-    prms.read_params(open(f, 'r'))
-    print 'Values:'
-    prms.print_values()
-    print "Nondims"
-    prms.print_nondim()
+    with open(f, 'r') as prm_file:
+        subs_list = BouySearch.get_prm_vals(prm_file).items()
+        print('Values:')
+        BouyParam.print_constants(subs_list)
+        print('Nondims:')
+        BouyParam.print_nondim(subs_list)

0d3b11648af33b57671f3a722b41e04625b7d984
Jonathan Robey <class4kayaker@gmail.com>
2016-09-16 14:57:15 -0700

    Add tests for all input elements

--- tests/test_fragments.py
+++ tests/test_fragments.py
@@ -2,10 +2,12 @@ import sci_parameter_utils.fragment as frag
 
 
 class TestInputInt:
+    tstr = 'int'
+
     def test_create(self):
         name = 'test'
         fmt = "{}"
-        elem = frag.TemplateElem.elem_by_type('int',
+        elem = frag.TemplateElem.elem_by_type(self.tstr,
                                               name,
                                               {}
                                               )
@@ -16,7 +18,59 @@ class TestInputInt:
     def test_create_w_fmt(self):
         name = 'test'
         fmt = "{:g}"
-        elem = frag.TemplateElem.elem_by_type('int',
+        elem = frag.TemplateElem.elem_by_type(self.tstr,
+                                              name,
+                                              {'fmt': fmt}
+                                              )
+
+        assert elem.name == name
+        assert elem.fmt == fmt
+
+
+class TestInputFloat:
+    tstr = 'float'
+
+    def test_create(self):
+        name = 'test'
+        fmt = "{}"
+        elem = frag.TemplateElem.elem_by_type(self.tstr,
+                                              name,
+                                              {}
+                                              )
+
+        assert elem.name == name
+        assert elem.fmt == fmt
+
+    def test_create_w_fmt(self):
+        name = 'test'
+        fmt = "{:g}"
+        elem = frag.TemplateElem.elem_by_type(self.tstr,
+                                              name,
+                                              {'fmt': fmt}
+                                              )
+
+        assert elem.name == name
+        assert elem.fmt == fmt
+
+
+class TestInputStr:
+    tstr = 'str'
+
+    def test_create(self):
+        name = 'test'
+        fmt = "{}"
+        elem = frag.TemplateElem.elem_by_type(self.tstr,
+                                              name,
+                                              {}
+                                              )
+
+        assert elem.name == name
+        assert elem.fmt == fmt
+
+    def test_create_w_fmt(self):
+        name = 'test'
+        fmt = "{:g}"
+        elem = frag.TemplateElem.elem_by_type(self.tstr,
                                               name,
                                               {'fmt': fmt}
                                               )

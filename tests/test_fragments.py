ec06b989016f37a3d52ef915cc0d978e55b86a11
Jonathan Robey <class4kayaker@gmail.com>
2016-09-16 15:24:51 -0700

    Adjust test arrangement

--- tests/test_fragments.py
+++ tests/test_fragments.py
@@ -1,79 +1,33 @@
+import pytest
 import sci_parameter_utils.fragment as frag
 
 
-class TestInputInt:
-    tstr = 'int'
-
-    def test_create(self):
+@pytest.mark.parametrize("tstr", [
+    'int',
+    'float',
+    'str'
+])
+class TestInputElems:
+    def test_create(self, tstr):
         name = 'test'
         fmt = "{}"
-        elem = frag.TemplateElem.elem_by_type(self.tstr,
+        elem = frag.TemplateElem.elem_by_type(tstr,
                                               name,
                                               {}
                                               )
 
         assert elem.name == name
         assert elem.fmt == fmt
+        assert elem.get_dependencies() == set()
 
-    def test_create_w_fmt(self):
+    def test_create_w_fmt(self, tstr):
         name = 'test'
         fmt = "{:g}"
-        elem = frag.TemplateElem.elem_by_type(self.tstr,
+        elem = frag.TemplateElem.elem_by_type(tstr,
                                               name,
                                               {'fmt': fmt}
                                               )
 
         assert elem.name == name
         assert elem.fmt == fmt
-
-
-class TestInputFloat:
-    tstr = 'float'
-
-    def test_create(self):
-        name = 'test'
-        fmt = "{}"
-        elem = frag.TemplateElem.elem_by_type(self.tstr,
-                                              name,
-                                              {}
-                                              )
-
-        assert elem.name == name
-        assert elem.fmt == fmt
-
-    def test_create_w_fmt(self):
-        name = 'test'
-        fmt = "{:g}"
-        elem = frag.TemplateElem.elem_by_type(self.tstr,
-                                              name,
-                                              {'fmt': fmt}
-                                              )
-
-        assert elem.name == name
-        assert elem.fmt == fmt
-
-
-class TestInputStr:
-    tstr = 'str'
-
-    def test_create(self):
-        name = 'test'
-        fmt = "{}"
-        elem = frag.TemplateElem.elem_by_type(self.tstr,
-                                              name,
-                                              {}
-                                              )
-
-        assert elem.name == name
-        assert elem.fmt == fmt
-
-    def test_create_w_fmt(self):
-        name = 'test'
-        fmt = "{:g}"
-        elem = frag.TemplateElem.elem_by_type(self.tstr,
-                                              name,
-                                              {'fmt': fmt}
-                                              )
-
-        assert elem.name == name
-        assert elem.fmt == fmt
+        assert elem.get_dependencies() == set()

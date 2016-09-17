89f3cd5367c537b19e0da495892eb7cd8a42390a
Jonathan Robey <class4kayaker@gmail.com>
2016-09-17 09:27:58 -0700

    Add better pytest test support

--- setup.py
+++ setup.py
@@ -3,17 +3,23 @@ A utility to package subsets of large ASPECT PVD files to visualize elsewhere
 without access to the original filesystem.
 """
 
+import sys
 from setuptools import find_packages, setup
+
+needs_pytest = {'pytest', 'test'}.intersection(sys.argv)
+pytest_runner = ['pytest_runner'] if needs_pytest else []
 
 package = 'sci_parameter_utils'
 version = '0.2.0'
 dependencies = [
     'click',
     'pyyaml',
-    'sympy'
+    'sympy',
 ]
+setup_deps = [
+] + pytest_runner
 test_deps = [
-    'pytest'
+    'pytest',
 ]
 
 setup(
@@ -22,10 +28,11 @@ setup(
     package_dir={'': 'src'},
     packages=find_packages('src', exclude=['tests']),
     install_requires=dependencies,
+    setup_requires=setup_deps,
     tests_require=test_deps,
     entry_points={
         'console_scripts': [
             'sci_parameter_utils = sci_parameter_utils.cli:cli_main'
         ]
-    }
+    },
 )

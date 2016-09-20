06db006806d5e05445a9bf8b4055a632224ae3f9
Jonathan Robey <class4kayaker@gmail.com>
2016-09-19 18:43:26 -0700

    Improve tests for general.py

--- tests/test_general.py
+++ tests/test_general.py
@@ -3,44 +3,6 @@ import sci_parameter_utils.general as gen
 import sci_parameter_utils.parameter_file as prm_file
 
 
-class TrivialParser(prm_file.PFileParser):
-    @staticmethod
-    def lines(pfile):
-        linenum = 0
-        for l in pfile:
-            linenum += 1
-            l = l.strip()
-            if l == '':
-                yield prm_file.PFileLine(None,
-                                         None,
-                                         lnum=linenum)
-            if l.startswith('#'):
-                yield prm_file.PFileLine("Comment",
-                                         l[1:].strip(),
-                                         lnum=linenum)
-            else:
-                try:
-                    key, value = l.split(' ', 1)
-                except:
-                    raise ValueError('Bad key-val on line {}: {}'
-                                     .format(linenum, l))
-                yield prm_file.PFileLine("KeyValue",
-                                         prm_file.KeyValuePair(key, value),
-                                         lnum=linenum)
-
-    @staticmethod
-    def typeset_line(line):
-        if line.ltype is None:
-            return '\n'
-        if line.ltype == "Comment":
-            return "# {}\n".format(line.value)
-        elif line.ltype == "KeyValue":
-            return '{} {}'.format(line.key,
-                                  line.value)
-        else:
-            raise ValueError("Unknown linetype: {}".format(str(line)))
-
-
 @pytest.mark.parametrize("istr,values,ostr", [
     ("", {}, ""),
     ("", {'a': 'b'}, ""),
@@ -60,3 +22,29 @@ def test_replace(istr, values, ostr):
 ])
 def test_replace_tofmt(istr, values, ostr):
     assert ostr == gen.do_replace(istr, values, to_fmt=True)
+
+
+@pytest.mark.parametrize("istr,values,error", [
+    ("{{{a}}}", {'b': 'b'}, "No element a supplied"),
+    ("{{{a}}}{{{a}}}", {'b': 'b'}, "No element a supplied"),
+    ("{{{c}}}{{{a}}}", {'d': 'b', 'c': 'd'}, "No element a supplied"),
+])
+def test_replace_invalid(istr, values, error):
+    with pytest.raises(ValueError) as excinfo:
+        gen.do_replace(istr, values)
+    assert str(excinfo.value) == error
+
+
+@pytest.mark.par
+@pytest.mark.parametrize('sugg,ostr', [
+    ('out', 'out'),
+    ('test', 'test'),
+    ('{{{a}}}', '{a}'),
+])
+def test_fn_suggest_basic(parser, tmpdir, sugg, ostr):
+    fn = 'out'
+    l = prm_file.PFileLine.commentline("FN: {}".format(sugg))
+    tmpdir.join(fn).write(parser.typeset_line(l))
+
+    with tmpdir.join(fn).open('r') as f:
+        assert ostr == gen.get_fn_suggest(f, parser)
